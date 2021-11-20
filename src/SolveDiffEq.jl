d855ceb7fe8fea8b4182ab1dd166f04f8ebd7576
yctai <brianyjtai1994@gmail.com>
2021-11-21 04:43:36 +0800

    Add: `estimate_err`, `apply_step!`; Edit: `BulirschStoer`

--- src/SolveDiffEq.jl
+++ src/SolveDiffEq.jl
@@ -9,6 +9,41 @@ const MatO  = AbstractMatrix # Output Matrix
 const MatB  = AbstractMatrix # Buffer Matrix
 const MatIO = AbstractMatrix # In/Out Matrix
 
+const ATOL = 1.0e-12
+const RTOL = 1.0e-12
+
+import LinearAlgebra: BLAS
+
+if haskey(ENV, "BLAS_THREAD_NUM")
+    BLAS.set_num_threads(parse(Int, ENV["BLAS_THREAD_NUM"]))
+else
+    BLAS.set_num_threads(4)
+end
+
+export BulirschStoer, apply_step!
+
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+
+macro def(genre::Symbol, ex::Union{Expr,Symbol}, vars::Symbol...)
+    o = genre == :prop ? :(::) : genre == :vars ? :(=) : error("@def(genre = $genre, ...) is invalid.")
+    n = length(vars)
+    e = Vector{Expr}(undef, n)
+    @inbounds for i in 1:n
+        e[i] = Expr(o, vars[i], ex)
+    end
+    return Expr(:escape, Expr(:block, e...))
+end
+
+macro get(obj::Symbol, vars::Symbol...)
+    n = length(vars)
+    e = Vector{Expr}(undef, n)
+    @inbounds for i in 1:n
+        vari = vars[i]
+        e[i] = :($vari = $obj.$vari)
+    end
+    return Expr(:escape, Expr(:block, e...))
+end
+
 function unsafe_cpy!(des::AbstractArray, src::AbstractArray)
     @simd for i in eachindex(des)
         @inbounds des[i] = src[i]
@@ -17,26 +52,6 @@ function unsafe_cpy!(des::AbstractArray, src::AbstractArray)
 end
 
 apply_grad!(ηn::VecO, ηm::VecI, tn::Real, g!::Function) = g!(ηn, ηm, tn)
-
-# @code_warntype ✓
-function get_stepsize(ns::NTuple{LMAX,Int}, h::Real) where LMAX
-    if @generated
-        a = Vector{Expr}(undef, LMAX)
-        @inbounds for k in 1:LMAX
-            a[i] = :(h / ns[$i])
-        end
-        return quote
-            $(Expr(:meta, :inline))
-            @inbounds return $(Expr(:vect, a...))
-        end
-    else
-        hs = Vector{Float64}(undef, LMAX)
-        @simd for i in eachindex(hs)
-            @inbounds hs[i] = h / ns[i]
-        end
-        return hs
-    end
-end
 
 # @code_warntype ✓
 # Don't use `:tuple` here, NTuple will cause
@@ -65,6 +80,26 @@ function get_stepinv2(ns::NTuple{LMAX,Int}) where LMAX
             xs[k] = a
         end
         return xs
+    end
+end
+
+# @code_warntype ✓
+function get_stepsize(ns::NTuple{LMAX,Int}, h::Real) where LMAX
+    if @generated
+        a = Vector{Expr}(undef, LMAX)
+        @inbounds for k in 1:LMAX
+            a[i] = :(h / ns[$i])
+        end
+        return quote
+            $(Expr(:meta, :inline))
+            @inbounds return $(Expr(:vect, a...))
+        end
+    else
+        hs = Vector{Float64}(undef, LMAX)
+        @simd for i in eachindex(hs)
+            @inbounds hs[i] = h / ns[i]
+        end
+        return hs
     end
 end
 
@@ -105,32 +140,86 @@ function extrap2zero!(ys::VecO, yr::VecO, ym::MatI, bm::MatB, xs::VecI, ℓ::Int
     return nothing
 end
 
+# @code_warntype ✓
+function estimate_err(ys::VecI, yr::VecI, y0::VecI)
+    err = 0.0 # error
+    @inbounds for i in eachindex(y0)
+        scal = ATOL + RTOL * max(abs(y0[i]), abs(yr[i])) # scale
+        err += abs2((ys[i] - yr[i]) / scal)
+    end
+    return sqrt(err / length(y0))
+end
+
 struct BulirschStoer{LMAX}
-    ns::NTuple{LMAX,Int}
-    hs::Vector{Float64}
+    @def prop Vector{Float64} hs ηt ηm ηn yr # vector of reference
+    @def prop Matrix{Float64} ym bm # matrix of sequences, buffer of matrix
     xs::Vector{Vector{Float64}}
-    ηt::Vector{Float64}
-    ηm::Vector{Float64}
-    ηn::Vector{Float64}
-    yr::Vector{Float64} # vector of reference
-    ym::Matrix{Float64} # matrix of sequences
-    bm::Matrix{Float64} # buffer of matrix
+    ns::NTuple{LMAX,Int}
 
     BulirschStoer{13}(H::Real, ydim::Int) = BulirschStoer(H, ydim, (2,  6, 10, 14, 18, 22, 26, 30, 34, 38, 42, 46, 50))
     BulirschStoer{ 9}(H::Real, ydim::Int) = BulirschStoer(H, ydim, (2,  8, 14, 20, 26, 32, 38, 44, 50))
     BulirschStoer{ 7}(H::Real, ydim::Int) = BulirschStoer(H, ydim, (2, 10, 18, 26, 34, 42, 50))
 
     function BulirschStoer(H::Real, ydim::Int, ns::NTuple{LMAX,Int}) where LMAX
-        hs = get_stepsize(ns, H)
+        @def vars Vector{Float64}(undef, ydim)       ηt ηm ηn yr
+        @def vars Matrix{Float64}(undef, ydim, LMAX) ym bm
         xs = get_stepinv2(ns)
-        ηt = Vector{Float64}(undef, ydim)
-        ηm = Vector{Float64}(undef, ydim)
-        ηn = Vector{Float64}(undef, ydim)
-        yr = Vector{Float64}(undef, ydim)
-        ym = Matrix{Float64}(undef, ydim, LMAX)
-        bm = Matrix{Float64}(undef, ydim, LMAX)
-        return new{LMAX}(ns, hs, xs, ηt, ηm, ηn, yr, ym, bm)
+        hs = get_stepsize(ns, H)
+        return new{LMAX}(hs, ηt, ηm, ηn, yr, ym, bm, xs, ns)
     end
+end
+
+# @code_warntype ✓
+function apply_step!(ys::VecO, y0::VecI, t0::Real, g!::Function, o::BulirschStoer{LMAX}) where LMAX
+    @get o ns xs hs ηt ηm ηn ym yr bm
+
+    prevE = Inf # previous error
+    one2n = eachindex(ηt)
+
+    for ℓ in 1:LMAX
+        @inbounds hℓ = hs[ℓ]
+        @inbounds nℓ = ns[ℓ]
+        h2 = 2.0 * hℓ
+
+        unsafe_cpy!(ηm, y0)
+        apply_grad!(ηn, ηm, t0, g!)
+        BLAS.axpby!(1., ηm, hℓ, ηn)
+    
+        tj = t0 + hℓ; j = 1
+        while j < nℓ
+            apply_grad!(ηt, ηn, tj, g!)
+            BLAS.axpby!(1., ηm, h2, ηt)
+            @inbounds for i in one2n
+                ηm[i], ηn[i] = ηn[i], ηt[i]
+            end
+            tj += hℓ; j += 1
+        end
+    
+        apply_grad!(ηt, ηn, tj, g!)
+        @inbounds for i in one2n
+            ηt[i] = 0.5 * (ηn[i] + ηm[i] + hℓ * ηt[i])
+        end
+
+        if isone(ℓ)
+            @inbounds for i in one2n
+                ys[i] = ym[i,1] = ηt[i]
+                yr[i] = ym[i,2] = 0.0
+            end
+        else
+            @inbounds for i in one2n
+                ym[i,ℓ] = ηt[i]
+            end
+            extrap2zero!(ys, yr, ym, bm, xs, ℓ)
+        end
+
+        thisE = estimate_err(ys, yr, y0)
+        if thisE > prevE || iszero(thisE)
+            break
+        end
+        prevE = thisE
+    end
+
+    return nothing
 end
 
 end # module
