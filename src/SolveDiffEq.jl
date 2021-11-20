c166e92eb550ec00db39eda8ca2153bd474b60b5
yctai <brianyjtai1994@gmail.com>
2021-11-20 23:09:51 +0800

    Add: `extrap2zero`; Edit: `get_stepsize`, `get_stepinv2`

--- src/SolveDiffEq.jl
+++ src/SolveDiffEq.jl
@@ -10,21 +10,84 @@ const MatB  = AbstractMatrix # Buffer Matrix
 const MatIO = AbstractMatrix # In/Out Matrix
 
 # @code_warntype ✓
-function get_stepsize(ns::VecI{Int}, H::Real)
-    hs = similar(ns, Float64)
-    @simd for i in eachindex(hs)
-        @inbounds hs[i] = H / ns[i]
+function get_stepsize(ns::NTuple{KMAX,Int}, h::Real) where KMAX
+    if @generated
+        a = Vector{Expr}(undef, KMAX)
+        @inbounds for k in 1:KMAX
+            a[i] = :(h / ns[$i])
+        end
+        return quote
+            $(Expr(:meta, :inline))
+            @inbounds return $(Expr(:vect, a...))
+        end
+    else
+        hs = Vector{Float64}(undef, KMAX)
+        @simd for i in eachindex(hs)
+            @inbounds hs[i] = h / ns[i]
+        end
+        return hs
     end
-    return hs
 end
 
 # @code_warntype ✓
-function get_stepinv2(ns::VecI{Int})
-    xs = similar(ns, Float64)
-    @simd for i in eachindex(xs)
-        @inbounds xs[i] = 4 / abs2(ns[i])
+# Don't use `:tuple` here, NTuple will cause
+# unexpected allocations during the extrapolation.
+function get_stepinv2(ns::NTuple{KMAX,Int}) where KMAX
+    if @generated
+        a = Vector{Expr}(undef, KMAX-1)
+        for k in 1:KMAX-1
+            b = Vector{Expr}(undef, KMAX-k)
+            @inbounds for i in 1:KMAX-k
+                b[i] = :(abs2(ns[$i] / ns[$(i+k)]) - 1.0)
+            end
+            @inbounds a[k] = Expr(:vect, b...)
+        end
+        return quote
+            $(Expr(:meta, :inline))
+            @inbounds return $(Expr(:vect, a...))
+        end
+    else
+        xs = Vector{Vector{Float64}}(undef, KMAX-1)
+        for k in 1:KMAX-1
+            a = Vector{Float64}(undef, KMAX-k)
+            @inbounds for i in 1:KMAX-k
+                a[i] = abs2(ns[i] / ns[i+k]) - 1.0
+            end
+            xs[k] = a
+        end
+        return xs
     end
-    return xs
+end
+
+#=
+Extrapolate a polynomial τ0 + τ1 * h² + τ2 * h⁴ + ...
+by h[i] = h / n[i] where n[1] < n[2] < n[3] < ... as
+
+    f(h[1]²) = T[11] → T[12] → T[13]
+    f(h[2]²) = T[22] → T[23] /
+    f(h[3]²) = T[33] /
+=#
+function extrap2zero(xv::VecI, yv::VecI, bv::VecB, xs::VecI, n::Int)
+    one2n = eachindex(1:n)
+    @inbounds for i in one2n
+        iszero(xv[i]) && return yv[i]
+    end
+    Δx = Inf
+    yp = 0.0
+    @inbounds for i in one2n
+        δx = abs(xv[i])
+        δx < Δx && (Δx = δx; yp = yv[i])
+    end
+    @simd for i in one2n
+        @inbounds bv[i] = yv[i] - yp
+    end
+    for k in 1:n-1
+        @inbounds xk = xs[k]
+        @inbounds for i in 1:n-k
+            bv[i] += (bv[i] - bv[i+1]) / xk[i]
+        end
+    end
+    @inbounds return bv[1] + yp
 end
 
 end # module
